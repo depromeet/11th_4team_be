28068352d8fdd96925a2120733e61eb60418d37d
이찬진 <water0641@naver.com>
2022-04-02 17:55:22 +0900

    model update

--- models.dev/Chat.js
+++ models.dev/Chat.js
@@ -1,13 +1,28 @@
 // 채팅
-// ttl or room ref list
+// ttl  , 1000개 리스터형식
 
 const { Schema, model } = require('mongoose');
 
 const ChatSchema = new Schema(
   {
     room: { type: Schema.Types.ObjectId, ref: 'room', required: true },
+    inside: { type: Boolean, required: true },
+    type: { type: String, required: true },
+    //위치
+
     sender: { type: Schema.Types.ObjectId, ref: 'user', required: true },
     message: { type: String, required: true }
+
+    /*
+        chatList  : [
+            {
+                type: { type: String, required: true },
+                inside  : {type : Boolean ,required: true },
+                sender: { type: Schema.Types.ObjectId, ref: 'user', required: true },
+                message: { type: String, required: true }
+            }
+        ]
+    */
   },
   { timestamps: true }
 );
--- models.dev/ChatScrap.js
+++ models.dev/ChatScrap.js
@@ -3,11 +3,29 @@
 const { Schema, model } = require('mongoose');
 
 // 복사본 저장 해야함
+const chatScrap = new ChatScrap({ phoneNumber });
+chatScrap._id;
+
 const ChatScrapSchema = new Schema(
   {
+    user: { type: Schema.Types.ObjectId, ref: 'user', required: true },
+
     room: { type: Schema.Types.ObjectId, ref: 'room', required: true },
     sender: { type: Schema.Types.ObjectId, ref: 'user', required: true },
-    message: { type: String, required: true }
+    message: { type: String, required: true },
+
+    // 안에 있는지 정보
+    inside: { type: Boolean, required: true },
+    type: { type: String, required: true }
+
+    // chatScrapList  : [
+    //     {
+    //         type: { type: String, required: true },
+    //         inside  : {type : Boolean ,required: true },
+    //         sender: { type: Schema.Types.ObjectId, ref: 'user', required: true },
+    //         message: { type: String, required: true }
+    //     }
+    // ]
   },
   { timestamps: true }
 );
